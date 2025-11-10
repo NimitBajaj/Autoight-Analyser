131f896f07d17c30018169f602553fad708c0122
nimit bajaj <student@nimit.bajaj2005@gmail.com>
2025-11-10 22:58:41 +0530

    Cleaning repo

--- src/debug_imports.py
+++
@@ -1,5 +0,0 @@
-import importlib
-
-mod = importlib.import_module("src.parsers.legend_extractor")
-print("Loaded from:", mod.__file__)
-print("parse_legend_terms available:", hasattr(mod, "parse_legend_terms"))
